c87218b8d8e5167cd2a1fa261e6c5e63cc2ec201
francobenettini <franbenettini@gmail.com>
2022-02-10 17:25:05 -0300

    add

--- js/sweetAlert.js
+++ js/sweetAlert.js
@@ -1,5 +1,17 @@
+const { default: swal } = require("sweetalert");
 
-
-$("#nav-bar-cta").click(function(){
-    swal.fire("Ejemplo basico de Sweet alert 2");
-});
+function sweet(){
+    Swal.fire({
+        title: 'Custom width, padding, color, background.',
+        width: 600,
+        padding: '3em',
+        color: '#716add',
+        background: '#fff url(/images/trees.png)',
+        backdrop: `
+          rgba(0,0,123,0.4)
+          url("/images/nyan-cat.gif")
+          left top
+          no-repeat
+        `
+      })
+};
