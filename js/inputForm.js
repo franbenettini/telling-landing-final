10449dbc3bbb97889a6f00a7c9da523a2a8f3e9f
LuObreg <LuObreg@users.noreply.github.com>
2022-02-22 16:13:23 -0300

    message sending fix

--- js/inputForm.js
+++ js/inputForm.js
@@ -1,29 +1,22 @@
-let CompleteName;
-let email;
-let text;
-        function getField(fieldSelected){
-            CompleteName = document.getElementById(fieldSelected);
-            email = document.getElementById(fieldSelected);
-            text = document.getElementById(fieldSelected);
-        }
-        const formC = document.getElementById('formComplete');
-        
-
-        const scriptURL = 'https://script.google.com/macros/s/AKfycbwf3R5QIB1O2uNOXrn5CrJQF--1W3yVU0Xs_POgoTmKx3cXDkxIzxnG0d1XIHpimUJr8Q/exec'
-        formC.onsubmit = (e) => {
-            let formData = new FormData();
-            formData.append("CompleteName", CompleteName.value)
-            formData.append("email", email.value)
-            formData.append("text", text.value)
-            e.preventDefault();
-            if(email.value){
-                 fetch(scriptURL, { method: 'POST', body: formData})
-                .then(response => {
-                console.log('Success!', response);
-                email.value = "";
-                sweet("Muchas gracias por ser parte", "Pronto vas a recibir novedades sobre Telling");
-            })
-            .catch(error => console.error('Error!', error.message))
-            }
-        }
-        
+let CompleteName = document.getElementById("nombre");
+let mail = document.getElementById("mail");
+let text = document.getElementById("mensaje");
+       
+const formC = document.getElementById('formComplete');
+const URL = 'https://script.google.com/macros/s/AKfycbwf3R5QIB1O2uNOXrn5CrJQF--1W3yVU0Xs_POgoTmKx3cXDkxIzxnG0d1XIHpimUJr8Q/exec'
+formC.onsubmit = (e) => {
+    let formData = new FormData();
+    formData.append("CompleteName", CompleteName.value)
+    formData.append("email", mail.value)
+    formData.append("text", text.value)
+    e.preventDefault();
+    if(mail.value){
+        fetch(URL, { method: 'POST', body: formData})
+        .then(response => {
+        console.log('Success!', response);
+        mail.value = "";
+        sweet("Recibimos tu mensaje", "Pronto vas a recibir novedades sobre Telling");
+    })
+    .catch(error => console.error('Error!', error.message))
+    }
+}
