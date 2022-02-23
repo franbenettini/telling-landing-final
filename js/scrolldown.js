a7d355610b484230ccd98b039ee2b4522d655eb9
francobenettini <franbenettini@gmail.com>
2022-02-23 17:52:42 -0300

    scroll menu

--- js/scrolldown.js
+++ js/scrolldown.js
@@ -1,19 +1,33 @@
 $(document).ready(function(){
-    $('#menuSobre').click(function(){
-        $('#sobreNosotros').animatescroll({padding:-345})
-    });
 
-    $('#menuFunciones').click(function(){
-        $('#funcionalidades').animatescroll({padding:75})
-    });
 
-    $('#menuEquipo').click(function(){
-        $('#equipo').animatescroll({padding:7})
-    });
+    $(".menuFunciones").click(function() {
+        $('html, body').animate({
+        scrollTop: $("#funcionalidades").offset().top -50
+        }, 700);
+        $('.hamburger').toggleClass("is-active");
+       });
 
-    $('#menuContacto').click(function(){
-        $('#contacto').animatescroll({padding:5})
-    });
+    $(".menuSobre").click(function() {
+        $('html, body').animate({
+        scrollTop: $("#sobreNosotros").offset().top -80
+        }, 700);
+        $('.hamburger').toggleClass("is-active");
+       });
+
+    $(".menuEquipo").click(function() {
+        $('html, body').animate({
+        scrollTop: $("#equipo").offset().top -40
+        }, 700);
+        $('.hamburger').toggleClass("is-active");
+       });
+
+    $(".menuContacto").click(function() {
+        $('html, body').animate({
+        scrollTop: $("#contacto").offset().top -55
+        }, 700);
+        $('.hamburger').toggleClass("is-active");
+       });
 
     //Para que el menu desplegable se oculte automáticamente luego de hacer click (en móviles)
     $('.navbar-nav>li>a').on('click', function(){
